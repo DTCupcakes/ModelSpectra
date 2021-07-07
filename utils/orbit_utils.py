0b51930c2db19b5038f8422cc4d8d82cddbcaf06
DTCupcakes <73225522+DTCupcakes@users.noreply.github.com>
2021-07-07 17:55:06 +1000

    Update orbit_utils.py
    Add comments

--- utils/orbit_utils.py
+++ utils/orbit_utils.py
@@ -1,14 +1,14 @@
 import numpy as np
 import utils.img_utils as img
 
-'''
-Constants
+''' Constants
 - All units are in cgs
 '''
+
 pi = np.pi
 
 c = 3e10 # Speed of light
-cms_to_kms = 1e-5
+cms_to_kms = 1e-5 # Convert cm to km
 
 G = 6.67e-8 # Gravitational constant
 R_sol = 6.9634e10 # Solar radius in cgs units
@@ -21,33 +21,63 @@ def GR_to_kms(v):
     v *= cms_to_kms
     return v
 
-'''
-Orbit integration functions
-'''
+''' Orbit integration functions '''
+
 def acc(x):
-    # Get acc. from pos.
-    r = np.sqrt(x[0]**2+x[1]**2)
-    a = (-G*WD_mass/r**3)*x
+    '''Get gravitational accerlation from position
+    
+        Parameters:
+        x -> Position
+        
+        Returns:
+        a -> Acceleration
+    '''
+    
+    r = np.sqrt(x[0]**2+x[1]**2) # Calculate distance from larger body
+    a = (-G*WD_mass/r**3)*x # Calculate acceleration
+    
     return a
 
 def acc_3D(x):
-    # Get acc. from pos.
-    r = np.sqrt(x[0]**2+x[1]**2+x[2]**2)
-    a = (-G*WD_mass/r**3)*x
+    '''Get gravitational accerlation from position in 3D
+    
+        Parameters:
+        x -> Position
+        
+        Returns:
+        a -> Acceleration
+    '''
+    
+    r = np.sqrt(x[0]**2+x[1]**2+x[2]**2) # Calculate distance from larger body
+    a = (-G*WD_mass/r**3)*x # Calculate acceleration
+
     return a
 
 def integrate_orbit(semia, e, phase=0):
-    '''Add phase to initial x and v'''
-    # Integrate over an orbit with parameters semia, e and return velocities
-    semia = semia*R_sol
-    n_points = 1000
-    period = 2*pi*np.sqrt(semia**3/(G*WD_mass))
-    dt = period/n_points # timestep
+    '''Integrate over an orbit and return orbital velocities
+        
+        Parameters:
+        semia -> Semi-major axis
+        e -> Eccentricity
+        phase -> Phase angle (f)
+        
+        Returns:
+        vx_n -> Velocities in the x direction
+        vy_n -> Velocities in the y direction
+    '''
+    
+    semia = semia*R_sol # Convert semi-major axis to solar radii
+    n_points = 1000 # Number of points for integration
+    period = 2*pi*np.sqrt(semia**3/(G*WD_mass)) # Orbit period
+    dt = period/n_points # Timestep
     r0 = semia*(1-e**2)/(1+e*np.cos(pi-phase)) # Initial value of r
+    
+    # Set initial position and velocity
     x = np.array([r0,0]) # Initial position
     v = np.array([0,np.sqrt(G*WD_mass*(2/r0-1/semia))]) # Initial velocity
-    vx_n = np.zeros(n_points)
-    vy_n = np.zeros(n_points)
+    
+    vx_n = np.zeros(n_points) # Velocities in the x direction
+    vy_n = np.zeros(n_points) # Velocities in the y direction
     for n in range(n_points):
         # Integrate using the leapfrog method
         a = acc(x)
@@ -57,26 +87,45 @@ def integrate_orbit(semia, e, phase=0):
         v = v + 0.5*dt*a
         vx_n[n] = v[0]*cms_to_kms
         vy_n[n] = v[1]*cms_to_kms
+        
     return vx_n, vy_n
 
 def integrate_orbit_3D(semia, e, i, O, w, f):
-    #Campbell elements
-    #semia should be in R_Sun
-    #Angles should be in degrees 
-    semia = semia*R_sol
-    n_points = 1000
-    period = 2*pi*np.sqrt(semia**3/(G*WD_mass))
+    '''Integrate over an orbit in 3D and return orbital velocities
+        - Campbell elements
+        - semia should be in R_Sun
+        - Angles should be in degrees 
+        
+        Parameters:
+        semia -> Semi-major axis
+        e -> Eccentricity
+        i -> Inclination
+        O -> Longitude of the ascending node
+        w -> Argument of periapsis
+        f -> True anomaly
+        
+        Returns:
+        vx_n -> Velocities in the x direction
+        vy_n -> Velocities in the y direction
+    '''
+
+    semia = semia*R_sol # Convert semi-major axis to solar radii
+    n_points = 1000 # Number of points for integration
+    period = 2*pi*np.sqrt(semia**3/(G*WD_mass)) # Period of the orbit
     dt = period/n_points # timestep
+    
     a = semia
     ecc = e
     omega = w*np.pi/180.
     # our conventions here are Omega is measured East of North
     big_omega = O*np.pi/180. + 0.5*np.pi
     inc = i*np.pi/180.
+    theta = f*np.pi/180.
+    
     # get eccentric anomaly from true anomaly
     # (https://en.wikipedia.org/wiki/Eccentric_anomaly#From_the_true_anomaly)
-    theta = f*np.pi/180.
     E = np.arctan2(np.sqrt(1. - ecc**2)*np.sin(theta),(ecc + np.cos(theta)))
+    
     # Positions in plane (Thiele-Innes elements)
     P = np.zeros(3)
     Q = np.zeros(3)
@@ -86,16 +135,20 @@ def integrate_orbit_3D(semia, e, i, O, w, f):
     Q[0] = -np.sin(omega)*np.cos(big_omega) - np.cos(omega)*np.cos(inc)*np.sin(big_omega)
     Q[1] = -np.sin(omega)*np.sin(big_omega) + np.cos(omega)*np.cos(inc)*np.cos(big_omega)
     Q[2] = np.sin(inc)*np.cos(omega)
+    
     term1 = np.cos(E)-ecc
     term2 = np.sqrt(1.-(ecc*ecc))*np.sin(E)
     E_dot = np.sqrt(G*WD_mass/(a**3))/(1.-ecc*np.cos(E))
+    
     # Rotating everything
     # Set the positions for the primary and the secondary
-    x = a*(term1*P + term2*Q)
+    x = a*(term1*P + term2*Q) # Initial position
+    
     # Set the velocities
-    v = -a*np.sin(E)*E_dot*P + a*np.sqrt(1.-(ecc*ecc))*np.cos(E)*E_dot*Q
-    vx_n = np.zeros(n_points)
-    vy_n = np.zeros(n_points)
+    v = -a*np.sin(E)*E_dot*P + a*np.sqrt(1.-(ecc*ecc))*np.cos(E)*E_dot*Q # Initial velocity
+    
+    vx_n = np.zeros(n_points) # Velocities in the x direction
+    vy_n = np.zeros(n_points) # Velocities in the y direction
     a = acc_3D(x)
     for n in range(n_points):
         # Integrate using the leapfrog method
@@ -105,72 +158,165 @@ def integrate_orbit_3D(semia, e, i, O, w, f):
         v = v + 0.5*dt*a
         vx_n[n] = v[0]*cms_to_kms
         vy_n[n] = v[1]*cms_to_kms
+        
     return vx_n, vy_n
 
 def get_model_3D(alpha, semia, e, i, O, w, f):
+    ''' Get velocity values for an orbit with given parameters
+        
+        Parameters:
+        alpha -> Orbital angles in velocity space
+        semia -> Semi-major axis
+        e -> Eccentricity
+        i -> Inclination
+        O -> Longitude of the ascending node
+        w -> Argument of periapsis
+        f -> True anomaly
+        
+        Returns: Velocity magnitudes for each value of alpha
+    '''
+    
+    # Get velocities in the x and y directions
     vx, vy = integrate_orbit_3D(semia, e, i, O, w, f)
-    v_mag = np.sqrt(vx**2 + vy**2)
-    v_angle = np.arctan2(vy, vx)
+    
+    # Convert x and y velocities to angles and magnitudes
+    v_mag = np.sqrt(vx**2 + vy**2) # Velocity magnitudes
+    v_angle = np.arctan2(vy, vx) # Velocity angles (alpha)
     #alpha = alpha + f
+    
+    # Interpolate velocity magnitudes for the values in alpha
     return np.interp(alpha, v_angle, v_mag, period=2*pi)
 
 def get_model_with_phase(alpha, semia, e, phase):
+    ''' Get velocity values for an orbit with given parameters
+        
+        Parameters:
+        alpha -> Orbital angles in velocity space
+        semia -> Semi-major axis
+        e -> Eccentricity
+        phase -> Phase angle (f)
+        
+        Returns: Velocity magnitudes for each value of alpha
+    '''
+    
+    # Get velocities in the x and y directions
     vx, vy = integrate_orbit(semia, e)
     # vx, vy = integrate_orbit_with_inc(semia, e)
-    v_mag = np.sqrt(vx**2 + vy**2)
-    v_angle = np.arctan2(vy, vx)
-    alpha = alpha + phase
+    
+    # Convert x and y velocities to angles and magnitudes
+    v_mag = np.sqrt(vx**2 + vy**2) # Velocity magnitudes
+    v_angle = np.arctan2(vy, vx) # Velocity angles (alpha)
+    alpha = alpha + phase # Add phase
+    
+    # Interpolate velocity magnitudes for the values in alpha
     return np.interp(alpha, v_angle, v_mag, period=2*pi)
 
-'''
-Functions to plot orbits
-'''
+''' Functions to plot orbits '''
+
 def plot_orbit_params(ax, semia, e, phase=0):
-    # Plot orbit with parameters semia and e in Cartesian coordinates
+    '''Plot orbit in Cartesian coordinates
+        
+        Parameters:
+        ax -> Plotting axis
+        semia -> Semi-major axis
+        e -> Eccentricity
+        phase -> Phase angle (f)
+    '''
+
     vx_n, vy_n = integrate_orbit(semia, e, phase=phase)
     ax.plot(vx_n, vy_n, 'r', label="orbit params")
 
 def plot_orbit_params_3D(ax, semia, e, i, O, w, f):
-    # Plot 3D orbit in Cartesian coordinates
+    '''Plot orbit (with orbit angles) in Cartesian coordinates
+        
+        Parameters:
+        ax -> Plotting axis
+        semia -> Semi-major axis
+        e -> Eccentricity
+        i -> Inclination
+        O -> Longitude of the ascending node
+        w -> Argument of periapsis
+        f -> True anomaly
+    '''
+    
     vx_n, vy_n = integrate_orbit_3D(semia, e, i, O, w, f)
     ax.plot(vx_n, vy_n, 'r', label="orbit params")
     
 def plot_orbit_params_polar(ax, semia, e, phase=0):
-    # Plot orbit with parameters semia and e in polar coordinates
+    '''Plot orbit in polar coordinates
+        
+        Parameters:
+        ax -> Plotting axis
+        semia -> Semi-major axis
+        e -> Eccentricity
+        phase -> Phase angle (f)
+    '''
+    
     vx_n, vy_n = integrate_orbit(semia, e, phase=phase)
+    
+    # Convert x and y velocities to polar coordinates
     v_mag_n, alpha_n = img.cart2polar(vx_n, vy_n)
     inds = alpha_n.argsort()
     alpha_n = alpha_n[inds]
     v_mag_n = v_mag_n[inds]
+    
     ax.plot(alpha_n, v_mag_n, 'r', label="orbit params")
 
 def plot_orbit_params_polar_3D(ax, semia, e, i, O, w, f):
-    # Plot 3D orbit in polar coordinates
+    '''Plot orbit (with orbit angles) in polar coordinates
+        
+        Parameters:
+        ax -> Plotting axis
+        semia -> Semi-major axis
+        e -> Eccentricity
+        i -> Inclination
+        O -> Longitude of the ascending node
+        w -> Argument of periapsis
+        f -> True anomaly
+    '''
+    
     vx_n, vy_n = integrate_orbit_3D(semia, e, i, O, w, f)
+    
+    # Convert x and y velocities to polar coordinates
     v_mag_n, alpha_n = img.cart2polar(vx_n, vy_n)
     inds = alpha_n.argsort()
     alpha_n = alpha_n[inds]
     v_mag_n = v_mag_n[inds]
+
     ax.plot(alpha_n, v_mag_n, 'r', label="orbit params")
         
 def plot_Kep_v(ax):
-    # Plot circles of Keplerian velocity at particular physical radii
+    '''Plot circles of Keplerian velocity at particular physical radii (in Cartesian coordinates)
+    
+        Parameters:
+        ax -> Plotting axis
+    '''
+    
     r = np.array([0.2, 0.64, 1.2, 2]) # Radii in solar radii
     v_mag_array = np.sqrt(G*WD_mass/(r*R_sol))*cms_to_kms # Convert r to kms
+    
     # Add circles to the tomogram
     step = 0
     linestyles = ['--', '-.', ':' , '-']
-    alpha = np.linspace(0, 2*pi, 100)
+    alpha = np.linspace(0, 2*pi, 100) # Angles in velocity space
     for v_mag in v_mag_array:
-        vx = v_mag*np.cos(alpha)
-        vy = v_mag*np.sin(alpha)
+        vx = v_mag*np.cos(alpha) # Velocity in the x direction
+        vy = v_mag*np.sin(alpha) # Velocity in the y direction
         label = str(r[step]) + r' $R_\odot$'
         ax.plot(vx, vy, linestyle=linestyles[step], color='w', label=label)
         step += 1
 
 def plot_Kep_v_3D(ax):
-    # Plot circles of Keplerian velocity at particular physical radii
+    '''Plot circles of Keplerian velocity at particular physical radii (in Cartesian coordinates)
+        - Plot circles at inclination of 17 degrees
+    
+        Parameters:
+        ax -> Plotting axis
+    '''
+    
     r = np.array([0.2, 0.64, 1.2, 2]) # Radii in solar radii
+    
+    # Add circles to the tomogram
     linestyles = ['--', '-.', ':' , '-']
     for i, rad in enumerate(r):
         vx, vy = integrate_orbit_3D(rad, 0, 17, 0, 0, 0) # Convert r to kms
